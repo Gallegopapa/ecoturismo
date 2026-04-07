126e2754ac038ca5a2ea1896e4c89854ace3fddd
Juan Jose Lopez Pineda <juanjolopin@gmail.com>
2026-04-07 08:11:16 -0500

    feat(US-REV-01): AC2 - validador de groserias (NoProfanity) implementado

--- app/Http/Controllers/API/ReviewController.php
+++ app/Http/Controllers/API/ReviewController.php
@@ -98,7 +98,7 @@ class ReviewController extends Controller
             'place_id' => 'required_without:ecohotel_id|nullable|exists:places,id',
             'ecohotel_id' => 'required_without:place_id|nullable|exists:ecohotels,id',
             'rating' => 'required|integer',
-            'comment' => ['nullable', 'string', 'max:500'],
+            'comment' => ['nullable', 'string', 'max:500', new \App\Rules\NoProfanity()],
         ], $messages);
 
         $review = Review::create([
