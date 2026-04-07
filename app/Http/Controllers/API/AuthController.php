fe55093ede60a2bb484bc70ab4af3c34fd5ef954
Juan Jose Lopez Pineda <juanjolopin@gmail.com>
2026-04-06 20:52:40 -0500

    feat(auth): implementation of AC2 - unique username validation (case-insensitive)

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -12,6 +12,24 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => [
+                'required',
+                'string',
+                'min:3',
+                'regex:/^[a-zA-Z0-9]+$/',
+                'unique:usuarios,name'
+            ],
+            'email' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
         // AC1: Data is valid -> returns 201 + token
         $user = Usuarios::create([
             'name' => $request->name,
